83546b773fce0c71f93cd9697e78ebe30f46cccf
Gusjarki <desm.g@yandex.ru>
2022-06-28 22:03:05 +0300

    Добавьте файлы проекта.

---
+++ HowMuchILoveYou/Program.cs
@@ -0,0 +1,19 @@
+﻿//Ваша цель  — определить, какую фразу девушки сказали бы о цветке с заданным количеством лепестков.
+
+Console.WriteLine(Kata.HowMuchILoveYou(int.Parse(Console.ReadLine()))) ;
+public class Kata
+{
+    public static string HowMuchILoveYou(int nb_petals)
+    {
+        string[] phrases = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
+        int result = 0;
+        for (int i = 0, j = 0; j < nb_petals; i++, j++)
+        {
+            if (i % 6 == 0)
+                i = 0;
+            result = i;
+        }
+        return phrases[result];
+
+    }
+}
